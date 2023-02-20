2754d6a332b451af8715ad0e743e1f8dd46c63bd
bennysong <bennysong@tencent.com>
2023-02-20 11:32:13 +0800

    feat: update

--- go/server/collection/collection.pb.gw.go
+++ go/server/collection/collection.pb.gw.go
@@ -99,6 +99,40 @@ func local_request_CollectionSvr_GetCollectionDetail_0(ctx context.Context, mars
 
 }
 
+func request_CollectionSvr_Lottery_0(ctx context.Context, marshaler runtime.Marshaler, client CollectionSvrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq LotteryReq
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Lottery(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_CollectionSvr_Lottery_0(ctx context.Context, marshaler runtime.Marshaler, server CollectionSvrServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq LotteryReq
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.Lottery(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterCollectionSvrHandlerServer registers the http handlers for service CollectionSvr to "mux".
 // UnaryRPC     :call CollectionSvrServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -152,6 +186,31 @@ func RegisterCollectionSvrHandlerServer(ctx context.Context, mux *runtime.ServeM
 		}
 
 		forward_CollectionSvr_GetCollectionDetail_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_CollectionSvr_Lottery_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/funstar.server.collection.CollectionSvr/Lottery", runtime.WithHTTPPathPattern("/collection/Lottery"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_CollectionSvr_Lottery_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CollectionSvr_Lottery_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -240,6 +299,28 @@ func RegisterCollectionSvrHandlerClient(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("POST", pattern_CollectionSvr_Lottery_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/funstar.server.collection.CollectionSvr/Lottery", runtime.WithHTTPPathPattern("/collection/Lottery"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CollectionSvr_Lottery_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CollectionSvr_Lottery_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -247,10 +328,14 @@ var (
 	pattern_CollectionSvr_GetCollectionList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"collection", "GetCollectionList"}, ""))
 
 	pattern_CollectionSvr_GetCollectionDetail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"collection", "GetCollectionDetail"}, ""))
+
+	pattern_CollectionSvr_Lottery_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"collection", "Lottery"}, ""))
 )
 
 var (
 	forward_CollectionSvr_GetCollectionList_0 = runtime.ForwardResponseMessage
 
 	forward_CollectionSvr_GetCollectionDetail_0 = runtime.ForwardResponseMessage
+
+	forward_CollectionSvr_Lottery_0 = runtime.ForwardResponseMessage
 )
